46106de77caca10e82b94082cc3fdd30ff37eebc
mikael-gaudet <mikael.gaudet@gmail.com>
2017-10-22 20:05:24 +0200

    Overhalling .Render

---
+++ render/content0.js
@@ -0,0 +1,18 @@
+function unhover0(element) { /* Makes images change on hover*/
+    element.setAttribute('src', 'Images/Computer/Comp1.jpg');
+      }
+function hover0(element) { /* Makes images change on hover*/
+    element.setAttribute('src', 'Images/Computer/Comp2.jpg');
+      }
+function unhover1(element) { /* Makes images change on hover*/
+    element.setAttribute('src', 'Images/Sofa/Sofa1.jpg');
+      }
+function hover1(element) { /* Makes images change on hover*/
+    element.setAttribute('src', 'Images/Sofa/Sofa2.jpg');
+      }
+function unhover2(element) { /* Makes images change on hover*/
+    element.setAttribute('src', 'Images/Space/Spac1.jpg');
+      }
+function hover2(element) { /* Makes images change on hover*/
+    element.setAttribute('src', 'Images/Space/Spac2.jpg');
+      }
